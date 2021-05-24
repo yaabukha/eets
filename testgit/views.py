50960556fc2b19e999f029dc7192ff281ff0c238
yaabukha <70237474+yaabukha@users.noreply.github.com>
2021-05-24 17:04:09 -0500

    add new test for OS

--- testgit/views.py
+++ testgit/views.py
@@ -5,15 +5,33 @@ Routes and views for the flask application.
 from datetime import datetime
 from flask import render_template
 from testgit import app
-
+from flask import Flask,render_template,request
+import time
+import os
 @app.route('/')
-@app.route('/home')
+@app.route('/home', methods = ['POST', 'GET'])
 def home():
+    if request.method == 'GET':
+         return render_template(
+        'index.html',
+        title='Home Page',
+        year=datetime.now().year,
+    )
+    if request.method == 'POST':
+        req = request.form
+        print(req.get("City"))
+        feedback = req.get("City")
+        time.sleep( 5 )
+        os.system('ls -ltr')
+        #feedback = "Resource Group has been created"
+        feedback = os.system('ls -ltr')
     """Renders the home page."""
     return render_template(
         'index.html',
         title='Home Page',
         year=datetime.now().year,
+        feedback=feedback
+       
     )
 
 @app.route('/contact')
@@ -35,3 +53,5 @@ def about():
         year=datetime.now().year,
         message='Your application description page.'
     )
+
+
