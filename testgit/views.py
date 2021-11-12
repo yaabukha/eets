6038bb43ebdc37d98808b9ba6dee299d251f978f
yaabukha <70237474+yaabukha@users.noreply.github.com>
2021-11-12 17:32:10 -0600

    Update views.py

--- testgit/views.py
+++ testgit/views.py
@@ -123,9 +123,11 @@ def upload_file():
 # Download API
 @app.route("/downloadfile/<filename>", methods = ['GET'])
 def download_file(filename):
-    filename = "file1.pcap"
+    filename = "captures.zip"
     return render_template('download.html',value=filename)
 @app.route('/return-files/<filename>')
 def return_files_tut(filename):
+    filename = "captures.zip"
     file_path = UPLOAD_FOLDER + filename
     return send_file(file_path, as_attachment=True, attachment_filename='')
+
