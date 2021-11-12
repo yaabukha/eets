141faee5a0370dcab5b0e3d0b12a92e5c76cc28a
yaabukha <70237474+yaabukha@users.noreply.github.com>
2021-11-12 17:30:41 -0600

    Update start.py

--- testgit/start.py
+++ testgit/start.py
@@ -1,36 +1,30 @@
 import subprocess
-import paramiko   
-from scp import SCPClient   
-   
-# declare credentials   
-# host = '10.22.1.4'   
-host =  subprocess.getoutput('dig EETS7302VM01.internal.cloudapp.net +short')
-username = 'eets7302'   
-password = 'EETS@123'   
-   
-# connect to server   
-con = paramiko.SSHClient()   
-con.load_system_host_keys()   
-con.connect(host, username=username, password=password)   
-transport = con.get_transport()
-channel = transport.open_session()
-   
-# copy the file accross  
-with SCPClient(con.get_transport()) as scp:   
-    scp.put('/home/eets7302/eets/testgit/remoteStart.py', '/home/eets7302/')   
+import paramiko
+from scp import SCPClient
 
-channel.exec_command('python3 /home/eets7302/remoteStart.py > /dev/null 2>&1 &')
 
 
 
 def startCapture():
     try:
-        hostName = subprocess.getoutput('hostname')
-        fileName = hostName + ".pcap"
-        subprocess.Popen(["sudo","tcpdump","-w",fileName])
+        host =  subprocess.getoutput('dig EETS7302VM01.internal.cloudapp.net +short')
+        username = 'eets7302'
+        password = 'EETS@123'
+        #connect to server
+        con = paramiko.SSHClient()
+        con.load_system_host_keys()
+        con.connect(host, username=username, password=password)
+        transport = con.get_transport()
+        channel = transport.open_session()
+
+        # copy the file accross
+        with SCPClient(con.get_transport()) as scp:
+         scp.put('/home/eets7302/eets/testgit/remoteStart.py', '/home/eets7302/')
+
+         channel.exec_command('python3 /home/eets7302/remoteStart.py > /dev/null 2>&1 &')
+
+
+
         return True
     except:
         return False
-
-
-startCapture()
