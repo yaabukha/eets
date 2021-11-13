5393602b549a9da6785bc3ebf7ac179ccfccde87
yaabukha <70237474+yaabukha@users.noreply.github.com>
2021-11-12 22:44:57 -0600

    Update remoteStart.py

--- testgit/remoteStart.py
+++ testgit/remoteStart.py
@@ -1,4 +1,5 @@
 import subprocess
+import requests
 
 
 
@@ -7,9 +8,19 @@ def startCapture():
         hostName = subprocess.getoutput('hostname')
         fileName = hostName + ".pcap"
         subprocess.Popen(["sudo","tcpdump","-w",fileName])
+        if hostName == "EETS7302VM01":
+            startHttpRequest()
         return True
     except:
         return False
+
+def startHttpRequest():
+    url = "http://eets7302vm02.internal.cloudapp.net"
+    for i in range(30):
+        res = requests.get(url)
+
+
+
 
 
 startCapture()
