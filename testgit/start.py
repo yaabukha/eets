94539150f02f3c325b69ac24d79a367e41d4f773
yaabukha <70237474+yaabukha@users.noreply.github.com>
2021-11-12 18:24:42 -0600

    Update start.py

--- testgit/start.py
+++ testgit/start.py
@@ -7,21 +7,31 @@ from scp import SCPClient
 
 def startCapture():
     try:
-        host =  subprocess.getoutput('dig EETS7302VM01.internal.cloudapp.net +short')
+        host1 =  subprocess.getoutput('dig EETS7302VM01.internal.cloudapp.net +short')
+        host2 =  subprocess.getoutput('dig EETS7302VM01.internal.cloudapp.net +short')
         username = 'eets7302'
         password = 'EETS@123'
         #connect to server
-        con = paramiko.SSHClient()
-        con.load_system_host_keys()
-        con.connect(host, username=username, password=password)
-        transport = con.get_transport()
-        channel = transport.open_session()
+        con1 = paramiko.SSHClient()
+        con1.load_system_host_keys()
+        con1.connect(host1, username=username, password=password)
+        con2 = paramiko.SSHClient()
+        con2.load_system_host_keys()
+        con2.connect(host2, username=username, password=password)
+        transport1 = con1.get_transport()
+        channel1 = transport1.open_session()
+        transport2 = con2.get_transport()
+        channel2 = transport2.open_session()
 
         # copy the file accross
-        with SCPClient(con.get_transport()) as scp:
+        with SCPClient(con1.get_transport()) as scp:
          scp.put('/home/eets7302/eets/testgit/remoteStart.py', '/home/eets7302/')
 
-         channel.exec_command('python3 /home/eets7302/remoteStart.py > /dev/null 2>&1 &')
+         channel1.exec_command('python3 /home/eets7302/remoteStart.py > /dev/null 2>&1 &')
+        with SCPClient(con2.get_transport()) as scp:
+         scp.put('/home/eets7302/eets/testgit/remoteStart.py', '/home/eets7302/')
+
+         channel2.exec_command('python3 /home/eets7302/remoteStart.py > /dev/null 2>&1 &')    
 
 
 
